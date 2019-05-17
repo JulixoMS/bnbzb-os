a26d802fb7c7bd148f8fb1d8cd3c6261c4859f9c
JulixoMS <39990944+JulixoMS@users.noreply.github.com>
2019-05-17 11:18:48 +0200

    Create bnbzbcorelib.h

---
+++ bnbzbcorelib.h
@@ -0,0 +1,10 @@
+// BNBZB kernel core library for apps
+
+#include "lib/klib.h"
+#include "lib/kybrd.h"
+#include "lib/kvga.h"
+#include "lib/kmouse.h"
+#include "lib/kfile.h"
+#include "lib/ksubprocess.h"
+
+#define __APP 0
